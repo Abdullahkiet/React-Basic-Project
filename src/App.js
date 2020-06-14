87642a3e92bb35e671b6843139d9db4dacb058f0
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-06-15 01:57:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          First React App made by Muhammad Abdullah
+          <br>Twitter handel: @MAbdullahDev</br>
         </p>
         <a
           className="App-link"
